9479885ec93efddf2975ce221e536ba33fe69d3e
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-07-19 15:23:14 +0200

    First interesting version of *second-order* recursive function `ackermann`.

---
+++ ackermann.scm
@@ -0,0 +1,88 @@
+
+; implementation of a macro for the original Ackermann function φ.
+
+(use matchable)
+
+    (define-syntax phi
+     (syntax-rules ()
+      ((_ m n 0) (+ m n))
+      ((_ m 0 1) 0)
+      ((_ m 0 2) 1)
+      ((_ m 0 p) m)
+      ((_ m n p) (phi m (phi m (sub1 n) p) (sub1 p)))))
+    
+    (define-syntax A
+     (syntax-rules ()
+      ((_ () ns) (a . ns))
+      ((_ (m . ms) ()) (A ms (a)))
+      ((_ (m . ms) (n . ns)) (A ms (A (m . ms) ns)))))
+
+    ;(phi 1 1 1)
+    
+    ;(A (a a) (a a a))
+
+    #;(let ((sexp '(A (a a) (a a a))))
+     (printf "~A is the ackermann value of ~A"
+      (let T ((sexp sexp))
+       (let ((expanded (expand sexp)))
+        (printf "~A is the expansion of ~A~N" expanded sexp)
+        (cond 
+         ((equal? sexp expanded) sexp)
+         (else (match (cdr expanded) 
+                ((A-macro (m . ms) '()) (T `(,A-macro ,ms (a))))
+                ((A-macro ms ns)
+                 (let* ((inner (cons 'A ns))
+                        (inner-expanded (expand inner)))
+                  ;(printf "~A is the inner expansion of ~A~N" inner-expanded inner)
+                  (cons (car expanded) (T inner)))))))))
+      sexp))
+
+(newline)
+(newline)
+
+    ;(let ((sexp '(A 1 2)))
+            (define ackermann-expander
+             (lambda (sexp tabs)
+              (printf "~A -> " sexp)
+              (match sexp
+               (('A 0 n)
+                (let ((arith `(add1 ,n)))
+                 (printf "~A, so go back to " arith)
+                 (eval arith)))
+               (('A m 0)
+                (let ((forward `(A ,(sub1 m) 1)))
+                 (printf "○ ~A~N~A" forward tabs)
+                 (ackermann-expander forward tabs))) 
+               (('A m n)
+                (let* ((inner `(A ,m ,(sub1 n)))
+                      (expanded-inner (ackermann-expander inner (string-append " " tabs)))
+                      (whole `(A ,(sub1 m) ,expanded-inner)))
+                 (printf "● ~A~N~A" whole tabs)
+                 (ackermann-expander whole (string-append "" tabs)))))))
+
+ 
+
+
+
+(newline)
+(newline)
+
+    (let* ((sexp '(A (a a) (a a a)))
+           (expanded (expand sexp)))
+     (display expanded))
+
+(newline)
+
+    (let* ((sexp '(A (a a) (a a a)))
+           (expanded (expand sexp)))
+     (display sexp)
+     (display expanded)
+     (display (expand (cdr expanded))))
+
+    (define-syntax B
+     (syntax-rules ()
+      ((_ (m . ms) ()) 'ms)))
+
+    ;(B (2 3 4) ())
+    
+    ;(A (a) ((a a) (a a)))
